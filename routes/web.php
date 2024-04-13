e5f0a428149c7c5d988b966f96fb9c292009fdcc
mikelesnr <mwanza.n.m@gmail.com>
2024-04-13 21:35:24 +0200

    listings controller added

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ListingController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Models\Listing;
@@ -16,17 +17,16 @@ use App\Models\Listing;
 */
 
 //All listings
-Route::get('/', function () {
-    return view('listings',[
-        'heading'=>'Latest Listings',
-        'listings'=>Listing::all()
-    ]);
-});
+Route::get('/', [ListingController::class, 'index']);
 
 //Single listing
-Route::get('/listings/{id}', function ($id) {
-    return view('listing',[
-        'heading'=>'Single Listing',
-        'listing'=>Listing::find($id)
-    ]);
-});
+Route::get('/listings/{listing}', [ListingController::class, 'show']);
+
+//Common Resource Routes:
+// index - Show all listings
+// show - Show single listing
+//create - Show form to create new listing
+//store -cStore new listing
+//edit - Show form to edit listing
+//update -Update listing
+//destroy - Delete listing
