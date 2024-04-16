0e4b9613ba02e9d1547d48761e94f0f9f1f1a6e4
mikelesnr <mwanza.n.m@gmail.com>
2024-04-17 00:16:59 +0200

    User listing realationship created

--- resources/views/components/listing-tags.blade.php
+++ resources/views/components/listing-tags.blade.php
@@ -5,7 +5,7 @@ $tags= explode(',',$tagsCsv);
 @endphp
 
 <ul class="flex">
-    @foreach($tags as $tag):
+    @foreach($tags as $tag)
     <li
         class="flex items-center justify-center bg-black text-white rounded-xl py-1 px-3 mr-2 text-xs"
     >
--- routes/web.php
+++ routes/web.php
@@ -21,35 +21,35 @@ use App\Models\Listing;
 Route::get('/', [ListingController::class, 'index']);
 
 //Show Create Form
-Route::get('/listings/create', [ListingController::class, 'create']);
+Route::get('/listings/create', [ListingController::class, 'create'])->middleware('auth');
 
 //Show Create Form
-Route::post('/listings', [ListingController::class, 'store']);
+Route::post('/listings', [ListingController::class, 'store'])->middleware('auth')->middleware('auth');
 
 //Show Edit Form
-Route::get('/listings/{listing}/edit', [ListingController::class, 'edit']);
+Route::get('/listings/{listing}/edit', [ListingController::class, 'edit'])->middleware('auth');
 
 //update -Update listing
-Route::put('/listings/{listing}', [ListingController::class, 'update']);
+Route::put('/listings/{listing}', [ListingController::class, 'update'])->middleware('auth');
 
 //delete -Delete listing
-Route::delete('/listings/{listing}', [ListingController::class, 'destroy']);
+Route::delete('/listings/{listing}', [ListingController::class, 'destroy'])->middleware('auth');
 
 //Single listing -> this route must be at the bottom because of the
 //wildcard {{listing}}
 Route::get('/listings/{listing}', [ListingController::class, 'show']);
 
 //show register create form
-Route::get('/register', [UserController::class, 'create']);
+Route::get('/register', [UserController::class, 'create'])->middleware('guest');
 
 //Create User 
 Route::post('/users', [UserController::class, 'store']);
 
 //log user out
-Route::post('/logout', [UserController::class, 'logout']);
+Route::post('/logout', [UserController::class, 'logout'])->middleware('auth');
 
 //show login form
-Route::get('/login', [UserController::class, 'login']);
+Route::get('/login', [UserController::class, 'login'])->name('login')->middleware('guest');
 
 //login form
 Route::post('/users/authenticate', [UserController::class, 'authenticate']);
