f94472a0d1812ccbf223af9532daf559c3c8825d
mikelesnr <mwanza.n.m@gmail.com>
2024-04-16 15:04:47 +0200

    Crud Functionality completed

--- app/Http/Controllers/ListingController.php
+++ app/Http/Controllers/ListingController.php
@@ -50,4 +50,40 @@ class ListingController extends Controller
 
         return redirect('/')->with('message',"Listing created successfully!");
     }
+
+    //Show Edit Form
+    public function edit(Listing $listing){
+        // dd($listing->title);
+        return view('listings.edit',['listing'=> $listing]);
+    }
+
+    //update listing data
+    public function update(Request $request, Listing $listing){
+        $formFields = $request->validate([
+            'title'=>'required',
+            'company'=>'required',
+            'location'=>'required',
+            'website'=>'required',
+            'email'=>['required','email'],
+            'tags'=>'required',
+            'description'=>'required'
+        ]);
+
+
+        if($request->hasFile('logo')) {
+            $formFields['logo'] = $request->file('logo')->store('logos', 'public');
+        }
+
+        // dd($formFields);
+        $listing->update($formFields);
+
+
+        return back()->with('message',"Listing created successfully!");
+    }
+
+    public function destroy(Listing $listing){
+        $listing->delete();
+        
+        return redirect('/')->with('message','Listing Deleted');
+    }
 }
